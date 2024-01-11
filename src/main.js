31dcdf2faea071a6233ef082448f38a1a5f4ecb9
TzuChingg <suger5769@gmail.com>
2024-01-11 20:46:09 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import '../node_modules/bootstrap';
+import './assets/main.scss'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+const app = createApp(App)
+
+app.use(VueAxios, axios)
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
