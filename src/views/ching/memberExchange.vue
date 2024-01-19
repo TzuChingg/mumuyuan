3f5511769afd627bdae88f9579cf237aa1a966f7
TzuChingg <suger5769@gmail.com>
2024-01-19 17:22:54 +0800

    feature: 會員中心全部頁面+Router設定

---
+++ src/views/ching/memberExchange.vue
@@ -0,0 +1,101 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="/navLogo.png" alt="" height="75" />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#">線上點餐</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#/reserve">立即訂位</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#/search">訂位/訂單查詢</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">常見問題</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#/login">登入/註冊</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-9">
+          <div class="row">
+            <div class="col-3 border-end">
+              <div class="logoCard d-flex justify-content-center mb-2">
+                <img class="rounded-circle mt-2" src="/logoCard.jpg" alt="" style="height: 100px" />
+              </div>
+              <div class="btn-group-vertical d-flex justify-content-center">
+                <button class="btn btn-outline-primary" type="button">
+                  <a href="#/memberCenter" class="d-block link-underline link-underline-opacity-0 "
+                    >會員中心</a
+                  >
+                </button>
+                <button class="btn btn-outline-primary" type="button">
+                  <a href="#/member/orderLog" class="d-block link-underline link-underline-opacity-0">歷史訂單</a>
+                </button>
+                <button class="btn btn-outline-primary" type="button">
+                  <a href="#/member/discount" class="d-block link-underline link-underline-opacity-0">我的優惠券</a>
+                </button>
+                <button class="btn btn-outline-primary active" type="button">
+                  <a href="#/member/exchange" class="d-block text-light link-underline link-underline-opacity-0">點數兌換</a>
+                </button>
+              </div>
+            </div>
+            <div class="col-9">
+              <h3 class="text-center">點數兌換</h3>
+              <hr />
+              <div class="row justify-content-center">
+                <div class="col-8 mb-4">
+                  <h5>點數兌換</h5>
+                  <hr />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 border-top">
+      <p class="col-md-4 ms-5 text-muted my-auto">© 2023 Company, Inc</p>
+      <ul class="nav col-md-4 justify-content-end">
+        <li class="nav-item"><a href="" class="nav-link px-2 text-muted">Home</a></li>
+        <li class="nav-item"><a href="" class="nav-link px-2 text-muted">Features</a></li>
+        <li class="nav-item"><a href="" class="nav-link px-2 text-muted">Pricing</a></li>
+        <li class="nav-item"><a href="" class="nav-link px-2 text-muted">FAQs</a></li>
+        <li class="nav-item"><a href="" class="nav-link px-2 text-muted">About</a></li>
+      </ul>
+    </footer>
+  </template>
+  
+  <style lang="scss">
+  button:hover {
+    a {
+      color: white;
+    }
+  }
+  .questionIcon:hover {
+    cursor: pointer;
+  }
+  </style>
+  
--- src/views/ching/memberOrderLog.vue
+++ src/views/ching/memberOrderLog.vue
@@ -46,13 +46,13 @@
               <img class="rounded-circle mt-2" src="/logoCard.jpg" alt="" style="height: 100px" />
             </div>
             <div class="btn-group-vertical d-flex justify-content-center">
-              <button class="btn btn-outline-primary active" type="button">
-                <a href="#/memberCenter" class="d-block text-light link-underline link-underline-opacity-0 "
+              <button class="btn btn-outline-primary " type="button">
+                <a href="#/memberCenter" class="d-block link-underline link-underline-opacity-0 "
                   >會員中心</a
                 >
               </button>
-              <button class="btn btn-outline-primary" type="button">
-                <a href="#/member/orderLog" class="d-block link-underline link-underline-opacity-0">歷史訂單</a>
+              <button class="btn btn-outline-primary active" type="button">
+                <a href="#/member/orderLog" class="d-block text-light link-underline link-underline-opacity-0">歷史訂單</a>
               </button>
               <button class="btn btn-outline-primary" type="button">
                 <a href="#/member/discount" class="d-block link-underline link-underline-opacity-0">我的優惠券</a>
@@ -63,72 +63,12 @@
             </div>
           </div>
           <div class="col-9">
-            <h3 class="text-center">會員中心</h3>
+            <h3 class="text-center">歷史訂單</h3>
             <hr />
             <div class="row justify-content-center">
               <div class="col-8 mb-4">
-                <h5>基本資料</h5>
+                <h5>歷史訂單</h5>
                 <hr />
-                <div class="mb-3 row">
-                  <label for="name" class="col-xxl-2 col-form-label">姓名：</label>
-                  <div class="col-sm-10">
-                    <input type="text" class="form-control" id="name" />
-                  </div>
-                </div>
-                <div class="mb-3 row">
-                  <label for="phone" class="col-xxl-2 col-form-label">手機：</label>
-                  <div class="col-sm-10">
-                    <input type="password" class="form-control" id="phone" />
-                  </div>
-                </div>
-                <div class="mb-3 row">
-                  <label for="email" class="col-xxl-2 col-form-label">信箱：</label>
-                  <div class="col-sm-10">
-                    <input type="password" class="form-control" id="email" />
-                  </div>
-                </div>
-                <div class="mb-3 row">
-                  <label for="email" class="col-xxl-3 col-form-label">會員點數：</label>
-                  <div class="col-sm-9">
-                    <p class="mt-2">
-                      10點
-                      <span
-                        class="questionIcon ms-3 bi-question-circle-fill"
-                        data-bs-toggle="tooltip"
-                        data-bs-placement="right"
-                        title="每消費100元集一點，用於優惠券兌換。"
-                      ></span>
-                    </p>
-                  </div>
-                </div>
-                <div class="buttonSave d-flex justify-content-center">
-                  <button class="btn btn-primary" type="button">儲存</button>
-                </div>
-              </div>
-              <div class="col-8 mb-4">
-                <h5>修改密碼</h5>
-                <hr />
-                <div class="mb-3 row">
-                  <label for="name" class="col-xxl-4 col-form-label">目前密碼：</label>
-                  <div class="col-sm-8">
-                    <input type="text" class="form-control" id="name" />
-                  </div>
-                </div>
-                <div class="mb-3 row">
-                  <label for="phone" class="col-xxl-4 col-form-label">新密碼：</label>
-                  <div class="col-sm-8">
-                    <input type="password" class="form-control" id="phone" />
-                  </div>
-                </div>
-                <div class="mb-3 row">
-                  <label for="name" class="col-xxl-4 col-form-label">重新輸入新密碼：</label>
-                  <div class="col-sm-8">
-                    <input type="text" class="form-control" id="name" />
-                  </div>
-                </div>
-                <div class="buttonSave d-flex justify-content-center">
-                  <button class="btn btn-primary" type="button">更改密碼</button>
-                </div>
               </div>
             </div>
           </div>
