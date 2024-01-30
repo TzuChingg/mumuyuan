212779b938f28c88a7815c2d25baabf4b31676f1
TzuChingg <suger5769@gmail.com>
2024-01-31 04:01:59 +0800

    feature: 滾動測試

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ import backend from './backend'
 
 //主頁
 import indexPage from '/src/views/ching/indexPage.vue'
-
+import notFoundPage from '/src/views/ching/notFound.vue'
 // 線上點餐
 import Menu from './claire/menuRouter'
 import Cart from './claire/cartRouter'
@@ -21,17 +21,32 @@ const routes = [
   { ...Menu },
   { ...Cart },
   // { path: '/', component: IndexTest },
-  { path: '/', component: indexPage },
+  { path: '/', name: 'home', component: indexPage },
+  { path: '/FAQ', name: 'FAQ', component: indexPage },
   { ...loginRouter },
   { ...reserveRouter },
   { ...searchRouter },
   ...backend,
-  ...memberRouter
+  ...memberRouter,
+  {path: '/:pathMatch(.*)*', name: 'notFound', component: notFoundPage}
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) 
+    {
+      return { selector: to.hash }; // <==== the important part
+    }
+
+
+    return savedPosition || new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ left: 0, top: 0 , behavior:'smooth'})
+      }, 400)
+    })
+  }
 })
 
 export default router
