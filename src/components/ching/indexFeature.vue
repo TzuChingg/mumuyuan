3adc04e1cebc5233fce31b798a642d332dd67e2a
TzuChingg <suger5769@gmail.com>
2024-02-05 02:04:13 +0800

    fix: 最新消息RWD

---
+++ src/components/ching/indexFeature.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="mt-5 mb-5 d-flex justify-content-center">
+                <img src="/首頁圖片/title-illustration-left-brown.png" alt="icon-left" />
+                <h2 class="text-primary text-center ms-3 me-3 my-auto">木木苑特色</h2>
+                <img src="/首頁圖片/title-illustration-right-brown.png" alt="icon-right" />
+            </div>
+        </div>
+    </div>
+    <div class="container bg-secondary">
+        <div class="row">
+            <div class="col-sm-12 d-flex justify-content-center flex-wrap mb-5">
+                <div class="feature position-relative">
+                    <img class="p-2 position-relative w-100" src="/首頁圖片/feature1.png" alt="直火燒烤不油炸" />
+                    <div class="featureText position-absolute">
+                        <p class="featureNum display-3 text-center text-light">01</p>
+                        <p class="fs-3 text-light">直火燒烤不油炸</p>
+                    </div>
+                </div>
+                <div class="feature position-relative">
+                    <img class="p-2 position-relative w-100" src="/首頁圖片/feature2.png" alt="富含膠原蛋白的秘密醬汁" />
+                    <div class="featureText2 position-absolute">
+                        <p class="featureNum display-3 text-center text-light">02</p>
+                        <p class="fs-3 text-light">富含膠原蛋白的秘密醬汁</p>
+                    </div>
+                </div>
+                <div class="feature position-relative">
+                    <img class="p-2 position-relative w-100" src="/首頁圖片/feature3.png" alt="餐點種類多樣化" />
+                    <div class="featureText position-absolute">
+                        <p class="featureNum display-3 text-center text-light">03</p>
+                        <p class="fs-3 text-light">餐點種類多樣化</p>
+                    </div>
+                </div>
+                <div class="feature position-relative">
+                    <img class="p-2 position-relative w-100" src="/首頁圖片/feature4.png" alt="乾淨明亮的空間" />
+                    <div class="featureText position-absolute">
+                        <p class="featureNum display-3 text-center text-light">04</p>
+                        <p class="fs-3 text-light">乾淨明亮的空間</p>
+                    </div>
+                </div>
+                <div class="feature position-relative">
+                    <img class="p-2 position-relative w-100" src="/首頁圖片/feature5.png" alt="優惠的價格 餐廳的享受" />
+                    <div class="featureText5 position-absolute">
+                        <p class="featureNum display-3 text-center text-light">05</p>
+                        <p class="fs-3 text-light">優惠的價格 餐廳的享受</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+.featureText {
+    top: 37%;
+    left: 28%;
+
+    .featureNum {
+        font-family: Georgia, 'Times New Roman', Times, serif;
+        font-style: italic;
+    }
+}
+
+.featureText2 {
+    top: 37%;
+    left: 15%;
+
+    .featureNum {
+        font-family: Georgia, 'Times New Roman', Times, serif;
+        font-style: italic;
+    }
+}
+
+.featureText5 {
+    top: 37%;
+    left: 15%;
+
+    .featureNum {
+        font-family: Georgia, 'Times New Roman', Times, serif;
+        font-style: italic;
+    }
+}</style>
---
+++ src/components/ching/indexNews.vue
@@ -0,0 +1,143 @@
+<template>
+    <div class="container-fuild bg-secondary overflow-hidden">
+        <div class="ms-0 me-0">
+            <div class="pt-5 mb-5 d-flex justify-content-center">
+                <img src="/首頁圖片/title-illustration-left-brown.png" alt="icon-left" />
+                <h2 class="text-primary text-center ms-3 me-3 my-auto">最新消息</h2>
+                <img src="/首頁圖片/title-illustration-right-brown.png" alt="icon-right" />
+            </div>
+        </div>
+        <div class="row justify-content-center position-relative ">
+            <div class="col-11 pb-5 ">
+                <div id="latestNews" class="carousel slide " data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="latestNewsCOl carousel-item "  v-for="(page, index) in temp" :key="index" :class="{ active : page[0] == true}">
+                            <div class="d-flex justify-content-around">
+                                <router-link :to="'/news/' + item.id" class="text-decoration-none" v-for="item in page[1]" :key="item.id">
+                                    <div class="card h-100" style="width: 19rem">
+                                        <img :src="item.newsImage" class="card-img-top" alt="..." />
+                                        <div class="card-body">
+                                            <p class="card-text fs-4">{{ item.newsTitle }}</p>
+                                            <p class="card-text">{{ item.newsContent }}</p>
+                                        </div>
+                                    </div>
+                                </router-link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button class="position-absolute newsBtn-left border-0" type="button" data-bs-target="#latestNews"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true" style="filter: invert(50%)"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="position-absolute newsBtn-right border-0" type="button" data-bs-target="#latestNews"
+                    data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true" style="filter: invert(50%)"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            news: [],
+            fullWidth: 0,
+            newsPage:1,
+            temp:[]
+        }
+    },
+    mounted() {
+        const vm = this;
+        this.fullWidth = window.innerWidth
+        window.onresize = () => {
+            vm.fullWidth = window.innerWidth;
+        }
+        this.getNews()
+    },
+
+    methods: {
+        getNews() {
+            this.$axios
+                .get(`/news`)
+                .then((result) => {
+                    this.news = result.data
+                    this.newsCarousel()
+                })
+                .catch((err) => {
+                    console.log(err.response)
+                })
+        },
+        newsCarousel(){
+            const newsCopy = JSON.parse(JSON.stringify(this.news))         
+            if ( this.fullWidth >= 1660) {
+                this.newsPage = 1
+                const len = parseInt(newsCopy.length/5)
+                this.newsPage += len
+                for (let i=0; i<this.newsPage; i++){
+                    this.temp.push([(i+1), newsCopy.slice((5)*i, (5)*(i+1))])
+                }
+            } else if (this.fullWidth >= 1337 & this.fullWidth < 1660) {
+                this.newsPage = 1
+                const len = parseInt(newsCopy.length/4)
+                this.newsPage += len;
+                for (let i=0; i<this.newsPage; i++){
+                    this.temp.push([(i+1), newsCopy.slice((4)*i, (4)*(i+1))])
+                }
+            }else if (this.fullWidth >= 1000 & this.fullWidth < 1337){
+                this.newsPage = 1
+                const len = parseInt(newsCopy.length/3)
+                this.newsPage += len;
+                for (let i=0; i<this.newsPage; i++){
+                    this.temp.push([(i+1), newsCopy.slice((3)*i, (3)*(i+1))])
+                }
+            }else if (this.fullWidth >= 690 & this.fullWidth < 1000) {
+                this.newsPage = 1
+                const len = parseInt(newsCopy.length/2)
+                this.newsPage += len;
+                for (let i=0; i<this.newsPage; i++){
+                    this.temp.push([(i+1), newsCopy.slice((2)*i, (2)*(i+1))])
+                }
+            }else {
+                this.newsPage = 1
+                const len = parseInt(newsCopy.length/1)
+                this.newsPage += len;
+                for (let i=0; i<this.newsPage; i++){
+                    this.temp.push([(i+1), newsCopy.slice((1)*i, (1)*(i+1))])
+                }
+            }
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.latestNewsCOl {
+
+    // max-width: 1820px;
+    .card-text {
+        // height: 150px;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+        text-overflow: ellipsis;
+        overflow: hidden;
+    }
+}
+
+.newsBtn-left {
+    top: 0;
+    bottom: 0;
+    left: 0%;
+}
+
+.newsBtn-right {
+    top: 0;
+    bottom: 0;
+    right: 0%;
+}
+</style>
