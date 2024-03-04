b0757a2eafb1adcb74fb85f5cd8989652268ae3c
TzuChingg <suger5769@gmail.com>
2024-03-04 23:04:07 +0800

    fix: 寄信格式

--- server.js
+++ server.js
@@ -149,7 +149,92 @@ wss.on('connection', function connection(ws) {
           from: 'mumuyuan1234@gmail.com',
           to: parsedMessage.mail,
           subject: '你好這是你的訂單號碼 ',
-          html: `你可以透過這個號碼查詢訂單狀態 ${parsedMessage.orderid}`,
+          attachments: [
+            {
+              filename: "mumulogo.jpg",
+              path: "https://drive.google.com/thumbnail?id=1bmLFoOtBjMTjbvX0ZJYB65WIlOd6yrm0", // 附件路徑
+              cid: "image_001", //cid可被郵件使用
+            },
+          ],
+          html: `
+        <!doctype html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <style>
+              .btn {
+                width: 100%;
+                display: inline-block;
+                font-weight: 400;
+                line-height: 1.5;
+                color: #212529;
+                text-align: center;
+                text-decoration: none;
+                vertical-align: middle;
+                cursor: pointer;
+                user-select: none;
+                background-color: transparent;
+                border: 1px solid transparent;
+                padding: 0.375rem 0.75rem;
+                font-size: 1rem;
+                border-radius: 0.25rem;
+              }
+              .text-light {
+                color: rgb(248, 249, 250);
+              }
+              .card{
+                padding: 1rem;
+                border: 2px solid #212529;
+              }
+              .card-body {
+                flex: 1 1 auto;
+              }
+              .card-title {
+                margin: 0.5rem 0;
+                font-weight: bolder;
+                font-size: 1.25rem;
+              }
+              .card-img-top{
+                width: 100%;
+              }
+              .card-text{
+                margin-bottom: 30px;
+              }
+              .mb-4 {
+                margin-bottom: 1.5rem;
+              }
+              .border {
+                border: 2px solid #212529;
+                padding: 0.8rem 1.2rem;
+              }
+            </style>
+            <title>Document</title>
+          </head>
+          <body>
+            <div class="card" style="width: 250px">
+              <img
+                src="cid:image_001"
+                class="card-img-top"
+                alt="logo error"
+              />
+              <div class="card-body">
+                <h5 class="card-title fw-bolder" style="letter-spacing: 1px; color: #a86a46">
+                  您的訂單編號為(可用於查詢訂單狀態)：
+                </h5>
+                <p class="card-text border">
+                  ${parsedMessage.orderid}
+                </p>
+                <div style="text-align: right; font-size:12px; color: #a86a46;">
+                  <p style="margin:0; font-weight: 400; letter-spacing: 1px;">感謝您的收取</P>
+                  <p style="margin:0 0 5px 0; font-weight: 400; letter-spacing: 1px;">祝您用餐愉快</P>
+                  <p style="margin:0; font-weight: 200;letter-spacing: 1px;">- 木木苑串燒專門店mumuyuan</P>
+                </div>
+              </div>
+            </div>
+          </body>
+        </html>
+        `
         }).then(info => {
           console.log({ info });
         }).catch(console.error);
